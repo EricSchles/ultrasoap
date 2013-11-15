ce957d42fcceb7201222a166cb6d6a316657ac27
William Stewart <william@yola.com>
2013-11-15 10:07:50 +0200

    Added creation of MS, A, and TXT records,  and removing of all records.

--- ultrasoap.py
+++ ultrasoap.py
@@ -7,6 +7,15 @@ from suds.wsse import Security, UsernameToken
 
 import ultratypes as types
 
+RECORD_TYPES = {
+    'A': '1',
+    'CNAME': '5',
+    'MX': '15',
+    'NS': '2',
+    'TXT': '16',
+}
+
+TTL = '86400'
 
 @contextmanager
 def udns_transaction(client):
@@ -67,6 +76,10 @@ class UltraDNSClient(object):
         self.client.set_options(wsse=security)
 
     @property
+    def factory(self):
+        return self.client.factory
+
+    @property
     def service(self):
         return self.client.service
 
@@ -98,3 +111,56 @@ class UltraDNSClient(object):
     def get_resource_records_of_zone(self, zone_name, rr_type=types.ALL):
         return self.service.getResourceRecordsOfZone(zoneName=zone_name,
                                                      rrType=rr_type)
+
+    @translate_exceptions
+    def create_record(self, zone_name, delegation_name, record_type):
+        resource_record = self.factory.create('ns5:ResourceRecord')
+        resource_record._DName = delegation_name
+        resource_record._TTL = TTL
+        resource_record._Type = RECORD_TYPES[record_type]
+        resource_record._ZoneName = zone_name
+
+        return resource_record
+
+    @translate_exceptions
+    def create_mx_record(self, zone_name, delegation_name, preference_value, mail_server,transaction_id=''):
+        resource_record = self.create_record(zone_name, delegation_name, 'MX')
+        resource_record.InfoValues._Info1Value = preference_value
+        resource_record.InfoValues._Info2Value = mail_server
+        return self.service.createResourceRecord(resourceRecord=resource_record,
+                                                 trasactionID=transaction_id)
+
+    @translate_exceptions
+    def create_a_record(self, zone_name, delegation_name, ip_address_v4, transaction_id=''):
+        resource_record = self.create_record(zone_name, delegation_name, 'A')
+        resource_record.InfoValues._Info1Value = ip_address_v4
+        return self.service.createResourceRecord(resourceRecord=resource_record,
+                                                 trasactionID=transaction_id)
+
+    @translate_exceptions
+    def create_txt_record(self, zone_name, delegation_name, char_str, transaction_id=''):
+        resource_record = self.create_record(zone_name, delegation_name, 'TXT')
+        resource_record.InfoValues._Info1Value = char_str
+        return self.service.createResourceRecord(resourceRecord=resource_record,
+                                                 trasactionID=transaction_id)
+
+    @translate_exceptions
+    def remove_records(self, zone_name, transaction_id=''):
+        existing_records = [rec for rec in self.get_resource_records_of_zone(zone_name)]
+
+        results = []
+
+        for rec in existing_records:
+            gu_id = rec._Guid
+            results.append(self.service.deleteResourceRecord(guid=gu_id,
+                                                             trasactionID=transaction_id))
+        return all(results)
+
+
+    # @translate_exceptions
+    # def create_ns_record(self, zone_name, zone_name_2, delegation_name, preference_value, transaction_id=''):
+    #     resource_record = self.create_record(zone_name, delegation_name, 'NS')
+    #     resource_record.InfoValues._Info1Value = zone_name_2
+    #     return self.service.createResourceRecord(resourceRecord=resource_record,
+    #                                              trasactionID=transaction_id)
+
