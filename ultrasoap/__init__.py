28ef7c247742b67be32462e721ad12f35f5b4299
William Stewart <william@synthasite.com>
2014-04-16 14:10:34 +0200

    Update package URL

--- ultrasoap/__init__.py
+++ ultrasoap/__init__.py
@@ -1,6 +1,6 @@
 """Python client for UltraDNS domain service."""
 
 __version__ = '0.1'
-__url__ = 'https://github.com/zoidbergwill/ultrasoap'
+__url__ = 'https://github.com/yola/ultrasoap'
 
 from ultrasoap import UltraDNSClient, types, UDNSException, ZoneNotFound
